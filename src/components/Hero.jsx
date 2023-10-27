9588b6246873fc57693237224833cffdd7a38de1
Aditya <pandeyaditya032002@gmail.com>
2023-10-27 14:51:17 +0530

    Final Links Fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,18 @@ const Hero = () => {
   return (
     <header className="w-full flex flex-col justify-center items-center">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <div className="flex items-center">
+          <img src={logo} alt="sumz_logo" className="w-16 object-contain" />
 
+          <h3 className=" font-extrabold leading-[1.15] text-black sm:text-4xl ">
+            Text
+            <span className="orange_gradient">Whisper</span>
+          </h3>
+        </div>
         <button
           type="button"
           onClick={() =>
-            window.open("https://github.com/pandeyaditya02?tab=repositories")
+            window.open("https://github.com/pandeyaditya02/ai_summarizer")
           }
           className="black_btn"
         >
@@ -24,8 +30,9 @@ const Hero = () => {
       </h1>
 
       <h2 className="desc">
-        Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+        Simplify your reading with TextWhisper, an open-source article
+        summarizer that transforms lengthy articles into clear and concise
+        summaries
       </h2>
     </header>
   );
